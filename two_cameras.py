f70898d88fbd94af7765f369a4d684060dcb2dd8
Gulnaz Gimaletdinova <gulnaz1024@yandex.ru>
2023-04-15 13:50:13 +0600

    add cameras on/off by key_pressed

--- two_cameras.py
+++ two_cameras.py
@@ -1,23 +1,40 @@
 import cv2
 
-USB_cam_capture = cv2.VideoCapture(0)
 built_in_cam_capture = cv2.VideoCapture(1)
+USB_cam_capture = cv2.VideoCapture(0)
+
+built_in_cam_switcher = 1
+USB_cam_switcher = 1
+
+built_in_cam_off = cv2.imread('built-in.jpg')
+USB_cam_off = cv2.imread('usb.jpg')
 
 while True:
     # Capture frame-by-frame
-    has_frame_USB_cam, frame_USB_cam = USB_cam_capture.read()
     has_frame_built_in_cam, frame_built_in_cam = built_in_cam_capture.read()
+    has_frame_USB_cam, frame_USB_cam = USB_cam_capture.read()    
 
-    if has_frame_USB_cam:
-    # Display the resulting frame
-        cv2.imshow('USB Cam', frame_USB_cam)
-
-    if has_frame_built_in_cam:
-    # Display the resulting frame
+    # Built-in Camera (Switcher key - 1) 
+    if has_frame_built_in_cam and built_in_cam_switcher % 2 == 1:
         cv2.imshow('Built-in Cam', frame_built_in_cam)
-        
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
+    else:
+        cv2.imshow('Built-in Cam', built_in_cam_off)
+
+    # USB Camera (Switcher key - 2) 
+    if has_frame_USB_cam and USB_cam_switcher % 2 == 1:
+        cv2.imshow('USB Cam', frame_USB_cam)
+    else:
+        cv2.imshow('USB Cam', USB_cam_off)
+
+    key = cv2.waitKey(1)
+    if key == ord('1'):
+        built_in_cam_switcher += 1
+    elif key == ord('2'):
+        USB_cam_switcher += 1
+    elif key == ord('R') or key == ord('r'):
+        print("RECORD STARTED")
+    elif key == ord('q'):
+        break    
 
 # When everything is done, release the capture
 USB_cam_capture.release()
